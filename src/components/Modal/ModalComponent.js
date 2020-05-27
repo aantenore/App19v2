b1dce73bdc92c7be149fcd66ae322af4df50477d
grownappptg <64636747+grownappptg@users.noreply.github.com>
2020-05-27 18:33:20 +0200

    modal component

---
+++ src/components/Modal/ModalComponent.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import { Modal, Text, TouchableHighlight, View, StyleSheet} from 'react-native'
+import { styles } from './styles';
+
+
+class ModalExample extends React.Component {
+   state = {
+      modalVisible: false,
+   }
+   setModalVisible(visible) {
+    this.setState({ modalVisible: visible });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Modal
+          animationType="slide"
+          transparent={false}
+          visible={this.state.modalVisible}
+          onRequestClose={() => {
+            alert('Modal has been closed.');
+          }}>
+          <View style={styles.modal}>
+            <View>
+              <Text>Hello World!</Text>
+
+              <TouchableHighlight
+                onPress={() => {
+                  this.setModalVisible(!this.state.modalVisible);
+                }}>
+                <Text>Hide Modal</Text>
+              </TouchableHighlight>
+            </View>
+          </View>
+        </Modal>
+
+        <TouchableHighlight
+          onPress={() => {
+            this.setModalVisible(true);
+          }}>
+          <Text>Show Modal</Text>
+        </TouchableHighlight>
+      </View>
+    );
+   }
+}
+export default ModalExample;
+
---
+++ src/components/Modal/styles.js
@@ -0,0 +1,19 @@
+import { StyleSheet} from 'react-native';
+
+export const styles = StyleSheet.create ({
+    container: {
+       alignItems: 'center',
+       backgroundColor: '#ede3f2',
+       padding: 100
+    },
+    modal: {
+       flex: 5,
+       alignItems: 'center',
+       backgroundColor: '#f7021a',
+       padding: 100
+    },
+    text: {
+       color: '#3f2949',
+       marginTop: 10
+    }
+ })
